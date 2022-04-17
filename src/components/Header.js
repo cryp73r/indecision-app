84444611cd830cb60e2c322632d1dcbbe8b3f7b6
Priyanshu Singh <priyanshusingh0082@gmail.com>
2022-04-18 00:09:27 +0530

    added link Src Code and credits

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const Header=(props)=>(
     <div className="header">
-        <div className="container">
-            <h1 className="header__title">{props.title}</h1>
-            {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+        <div className='container'>
+            <div className="header__sbs">
+                <div>
+                    <h1 className="header__title">{props.title}</h1>
+                    {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+                </div>
+                <div>
+                    <a className="header__logo" title="Source Code" href="https://github.com/cryp73r/indecision-app" target="_blank"><ion-icon name="logo-github"></ion-icon></a>
+                </div>
+            </div>
+        </div>
+        <div className="container header__credits">
+            <h2 className="header__subtitle">Crafted with ❤️ by CRYP73R</h2>
         </div>
     </div>
 );
